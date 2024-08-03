936dd87c420ae6938457f3a921b33520cfbd410c
chiao <chiaogu@gmail.com>
2024-08-03 02:45:15 -0700

    nested envelope

--- src/synth/config/amplitudeEnvelope.ts
+++ src/synth/config/amplitudeEnvelope.ts
@@ -1,20 +1,21 @@
 import * as Tone from '@src/tone';
-import { removeDefault } from '@src/utils/schema';
 import { calculateEnvelope } from '@src/utils/utils';
 import createConfig from '../createConfig';
 import createFxClass from '../createFxClass';
-import { zEnvelope, zInnerEnvelope } from './envelope';
+import { zEnvelope } from './envelope';
 import { zBaseSynthFx } from './shared';
 
 export const amplitudeEnvelopeConfig = createConfig(
   createFxClass(Tone.AmplitudeEnvelope),
-  zBaseSynthFx.merge(removeDefault(zEnvelope) as typeof zInnerEnvelope),
+  zBaseSynthFx.extend({
+    envelope: zEnvelope,
+  }),
   {
     trigger(node, src) {
       node.triggerAttackRelease(src.duration, src.delay);
     },
     setState(node, state, { duration }) {
-      node.set(calculateEnvelope(state, duration));
+      node.set(calculateEnvelope(state.envelope, duration));
     },
   },
 );
