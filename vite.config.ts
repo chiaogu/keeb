d6cfd8888b075df6511f9c0b80e67ece43064ac9
chiao <chiaogu@gmail.com>
2024-08-10 01:33:49 -0700

    fix gh page routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { defineConfig } from 'vite';
+
+console.log(path.resolve(__dirname, './404.html'));
 
 export default defineConfig({
   resolve: {
@@ -9,12 +11,16 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './assets'),
     },
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   root: path.resolve('./src'),
   build: {
     outDir: path.resolve('./dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, './src/index.html'),
+        404: path.resolve(__dirname, './src/404.html'),
+      },
+    },
   },
   base: '/keeb',
   server: {
