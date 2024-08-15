980b5ed79608eb93ee93277eace75cc3f78f47f6
chiao <chiaogu@gmail.com>
2024-08-15 01:32:09 -0700

    Keyboard style

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,10 +1,12 @@
+import { useDebounceCallback } from '@react-hook/debounce';
 import usePressedKeys from '@src/hooks/usePressedKeys';
+import usePreventDefaultTouchStart from '@src/hooks/usePreventDefaultTouchStart';
 import getKeyCodeLabel from '@src/keyboard/getKeyLabel';
 import getKeyWidth from '@src/keyboard/getKeyWidth';
 import { keys } from '@src/keyboard/keys';
 import { getValueBg, normalValueToBrightness } from '@src/utils/utils';
 
-const keySize = 44;
+const keySize = 50;
 
 export type KeyboardProps = {
   className?: string;
@@ -15,70 +17,85 @@ export type KeyboardProps = {
   highlightedKeys?: { [key: string]: number };
 };
 
-export default function Keyboard({
-  className,
+function Key({
+  keyCode,
+  keyIndex,
   onPress,
   onRelease,
   onClick,
   selectedKeys = [],
   highlightedKeys = {},
-}: KeyboardProps) {
+}: KeyboardProps & { keyCode: string; keyIndex: number }) {
   const { pressedKeys, press, release } = usePressedKeys();
 
+  const handlePress = useDebounceCallback(() => {
+    press(keyCode);
+    onPress?.(keyCode);
+  }, 50);
+  const handleRelease = () => {
+    release(keyCode);
+    onRelease?.(keyCode);
+  };
+  const pressed = pressedKeys.includes(keyCode);
+  const selected = selectedKeys.includes(keyCode);
+  const bgValue = normalValueToBrightness(highlightedKeys[keyCode]);
+  const bg = getValueBg(bgValue);
+  const color = bgValue > 0.5 ? 'black' : 'white';
   return (
-    <div className={`relative flex flex-col items-start ${className}`}>
-      <div className='pointer-events-none absolute size-full border-2 border-black'></div>
+    <div
+      key={`${keyCode}-${keyIndex}`}
+      className='cursor-pointer pb-1 pr-1'
+      style={{
+        height: keySize,
+        width: `${keySize * getKeyWidth(keyCode)}px`,
+      }}
+    >
+      <div
+        style={{
+          // filter: pressed ? 'invert(1)' : undefined,
+          // background: pressed ? (bgValue > 0.5 ? 'white' : 'black') : bg,
+          background: bg,
+          color: color,
+          boxShadow: `0 ${pressed ? 2 : 5}px 10px 0px rgba(0,0,0,${pressed ? 0.1 : 0.05})`,
+          transform: `scale(${pressed ? 0.8 : 1})`,
+          // transform: `translateY(${pressed ? 4 : 0}px)`,
+        }}
+        className='flex size-full select-none items-center justify-center'
+        onPointerDown={handlePress}
+        onPointerUp={handleRelease}
+        onClick={() => {
+          onClick?.(keyCode);
+        }}
+      >
+        <div
+          style={{
+            border: selected ? `2px solid ${color}` : undefined,
+          }}
+          className='flex size-11/12 items-center justify-center'
+          onPointerEnter={(e) => e.buttons > 0 && handlePress()}
+          onPointerLeave={(e) => e.buttons > 0 && handleRelease()}
+        >
+          {getKeyCodeLabel(keyCode).toLowerCase()}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Keyboard({ className, ...props }: KeyboardProps) {
+  const ref = usePreventDefaultTouchStart();
+
+  return (
+    <div
+      className={`relative flex flex-col items-start ${className}`}
+      ref={ref}
+    >
+      <div className='pointer-events-none absolute size-full'></div>
       {keys.map((row, rowIndex) => (
         <div key={`row-${rowIndex}`} className='flex'>
-          {row.map((key, keyIndex) => {
-            const handlePress = () => {
-              press(key);
-              onPress?.(key);
-            };
-            const handleRelease = () => {
-              release(key);
-              onRelease?.(key);
-            };
-            const pressed = pressedKeys.includes(key);
-            const selected = selectedKeys.includes(key);
-            const bgValue = normalValueToBrightness(highlightedKeys[key]);
-            const bg = getValueBg(bgValue);
-            const color = bgValue > 0.5 ? 'black' : 'white';
-            return (
-              <div
-                key={`${key}-${keyIndex}`}
-                style={{
-                  height: keySize,
-                  width: `${keySize * getKeyWidth(key)}px`,
-                  border:
-                    selectedKeys.length > 0 ? undefined : '0.5px solid black',
-                }}
-              >
-                <div
-                  style={{
-                    filter: pressed ? 'invert(1)' : undefined,
-                    background: bg,
-                    color: color,
-                  }}
-                  className='flex size-full select-none items-center justify-center'
-                  onMouseDown={handlePress}
-                  onMouseUp={handleRelease}
-                  onClick={() => onClick?.(key)}
-                >
-                  <div
-                    style={{
-                      border: selected ? `2px solid ${color}` : undefined,
-                    }}
-                    className='flex size-4/5 items-center justify-center'
-                    onMouseEnter={(e) => e.buttons > 0 && handlePress()}
-                    onMouseLeave={(e) => e.buttons > 0 && handleRelease()}
-                  >
-                    {getKeyCodeLabel(key).toLowerCase()}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {row.map((key, keyIndex) => (
+            <Key key={key} keyCode={key} keyIndex={keyIndex} {...props} />
+          ))}
         </div>
       ))}
     </div>
